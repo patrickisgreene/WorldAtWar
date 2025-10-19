4b06d88065f726260d4db02605e493ce2461fe1c
Patrick Greene <patrickisgreene@gmail.com>
2025-10-18 19:34:18 -0500

    Create rust project by adding utils crate & expected dependencies.

---
+++ crates/utils/src/consts.rs
@@ -0,0 +1,12 @@
+pub const EARTH_RADIUS: f64 = 6_371_000.0; // meters
+pub const MOON_RADIUS: f64 = 1_737_400.0; // meters
+pub const SUN_RADIUS: f64 = 696_340_000.0; // meters
+
+pub const SUN_TO_EARTH: f64 = 149_597_870_700.0; // meters (1 AU)
+pub const MOON_TO_EARTH: f64 = 384_400_000.0; // meters (average)
+
+pub const MOUNT_EVEREST: f64 = 8_849.0; // meters above sea level
+pub const MARIANA_TRENCH: f64 = -10_994.0; // meters below sea level
+
+pub const GEOSYNCHRONUS_ORBIT: f64 = 35_786_000.0; // meters above sea level
+pub const ISS_ORBIT: f64 = 408_000.0; // meters above sea level (average)
