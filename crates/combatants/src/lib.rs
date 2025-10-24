5dcdd41ac90fec7b889ffc25674bd21b6c273faa
Patrick Greene <patrickisgreene@gmail.com>
2025-10-24 00:39:37 -0500

    Add alliances

--- crates/combatants/src/lib.rs
+++ crates/combatants/src/lib.rs
@@ -5,3 +5,19 @@ mod combatant;
 pub use combatant::Combatant;
 pub use color::CombatantColor;
 pub use plugin::CombatantsPlugin;
+
+use bevy_ecs::prelude::*;
+use bevy_asset::{prelude::*, LoadedFolder};
+use bevy_reflect::TypePath;
+use serde::{Serialize, Deserialize};
+
+#[derive(Resource)]
+pub struct Alliances(pub Handle<LoadedFolder>);
+
+#[derive(Asset, TypePath, Debug, PartialEq, Serialize, Deserialize)]
+pub struct Alliance {
+    pub name_short: String,
+    pub name_long: Option<String>,
+    pub color: (f32, f32, f32),
+    pub countries: Vec<String>
+}
--- crates/combatants/src/plugin.rs
+++ crates/combatants/src/plugin.rs
@@ -1,17 +1,24 @@
 use bevy_app::prelude::*;
 use bevy_ecs::prelude::*;
+use bevy_asset::prelude::*;
 
 use waw_ron_asset::RonAssetPlugin;
 
-use crate::{Combatant, CombatantColor};
+use crate::{Alliance, Alliances, Combatant, CombatantColor};
 
 pub struct CombatantsPlugin;
 
 impl Plugin for CombatantsPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(RonAssetPlugin::<Combatant>::new("combatant.ron"))
-            .add_systems(Startup, temp_load_combatants);
+            .add_plugins(RonAssetPlugin::<Alliance>::new("alliance.ron"))
+            .add_systems(Startup, temp_load_combatants)
+            .add_systems(Startup, load_alliances);
     }
+}
+
+fn load_alliances(mut commands: Commands, assets: Res<AssetServer>) {
+    commands.insert_resource(Alliances(assets.load_folder("alliances")));
 }
 
 fn temp_load_combatants(mut commands: Commands) {
